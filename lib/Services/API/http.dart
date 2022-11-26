358939540f55179231de16716c78d85745657166
H4D3ZS <adingferrer@yahoo.com>
2022-11-26 22:45:17 +0800

    changes

--- lib/Services/API/http.dart
+++ lib/Services/API/http.dart
@@ -1,15 +1,18 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:progqou/Services/models.dart';
 
-String baseUrl = "https://programming-quotes-api.herokuapp.com/Quotes?count=10";
+// String baseUrl = "https://programming-quotes-api.herokuapp.com/Quotes?count=10";
 
-class QouteR {
-  String url = baseUrl;
+class QouteRepository {
+  String url = "https://programming-quotes-api.herokuapp.com/Quotes?count=10";
   Future<List<QouteModel>> getQoutes() async {
     Response response = await get(Uri.parse(url));
+    debugPrint(response.body);
     if (response.statusCode == 200) {
-      final List result = jsonDecode(response.body)['data'];
+      final List result = jsonDecode(response.body);
+      debugPrint(result.toString());
       return result.map(((e) => QouteModel.fromJson(e))).toList();
     } else {
       throw Exception(response.reasonPhrase);
