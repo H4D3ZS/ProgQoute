23742ef436e32c947b06ee84ffd599762bb5c07c
H4D3ZS <adingferrer@yahoo.com>
2022-11-26 16:57:32 +0800

    Changes

--- lib/Services/API/http.dart
+++ lib/Services/API/http.dart
@@ -1,44 +1,18 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:progqou/Services/models.dart';
 
-class API {
-  fetchData() async {
-    try {
-      var resp = await Dio().get(
-        "https://programming-quotes-api.herokuapp.com/Quotes?count=10",
-        options: Options(responseType: ResponseType.plain),
-      );
+String baseUrl = "https://programming-quotes-api.herokuapp.com/Quotes?count=10";
 
-      var responseBody = resp.data;
-
-      print(responseBody);
-
-      // print(respData.trim);
-
-      // var rsort = res.sort(res);
-    } catch (e) {
-      // Logger().d("$e , result");
+class QouteR {
+  String url = baseUrl;
+  Future<List<QouteModel>> getQoutes() async {
+    Response response = await get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final List result = jsonDecode(response.body)['data'];
+      return result.map(((e) => QouteModel.fromJson(e))).toList();
+    } else {
+      throw Exception(response.reasonPhrase);
     }
-  }
-}
-
-class Authors {
-  int? id;
-  String? name;
-  String? qoutes;
-
-  Authors({this.id, this.name, this.qoutes});
-
-  Authors.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    qoutes = json['qoutes'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['qoutes'] = this.qoutes;
-    return data;
   }
 }
